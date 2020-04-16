002d456d767d9885de4c674d3ea97b2b93d9c229
Brian Roberts <flicken-github@flicken.net>
2020-04-16 19:51:12 +0200

    Add example of date range picker

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { DateRangePicker } from 'react-date-range';
+
+import { addDays } from "date-fns";
+import { useState } from 'react';
+
+
+
 function App() {
+  const [state, setState] = useState({
+    selection1: {
+      startDate: addDays(new Date(), 1),
+      endDate: null,
+      key: 'selection1'
+    },
+    selection2: {
+      startDate: addDays(new Date(), 4),
+      endDate: addDays(new Date(), 8),
+      key: 'selection2'
+    },
+    selection3: {
+      startDate: addDays(new Date(), 8),
+      endDate: addDays(new Date(), 10),
+      key: 'selection3',
+      autoFocus: false
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateRangePicker
+        onChange={item => setState({ ...state, ...item })}
+        ranges={[state.selection1, state.selection2, state.selection3]}
+      />
     </div>
   );
 }

c4b147cf0ce7d5354cb743c95fcab41c4259aaec
Brian Roberts <flicken-github@flicken.net>
2020-04-17 10:07:44 +0200

    Allow unlimited selections of dates.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
-import { DateRangePicker } from 'react-date-range';
+import { DateRange } from 'react-date-range';
 
-import { addDays } from "date-fns";
 import { useState } from 'react';
 
+import {DateTime} from "luxon";
 
+function emptyDate(index) {
+  let maybeEpoch = new Date(1970, 1, 31)
+  return {
+    startDate: maybeEpoch,
+    endDate: maybeEpoch,
+    key: '' + index
+  }
+}
 
 function App() {
   const [state, setState] = useState({
-    selection1: {
-      startDate: addDays(new Date(), 1),
-      endDate: null,
-      key: 'selection1'
-    },
-    selection2: {
-      startDate: addDays(new Date(), 4),
-      endDate: addDays(new Date(), 8),
-      key: 'selection2'
-    },
-    selection3: {
-      startDate: addDays(new Date(), 8),
-      endDate: addDays(new Date(), 10),
-      key: 'selection3',
-      autoFocus: false
-    }
+    selections: [emptyDate(0),
+    ]
   });
 
+  let onChange = item => {
+    console.log("Selected")
+    let selection = Object.values(item)[0]
+    console.log(selection)
+    let i = parseInt(selection.key)
+    state.selections[i] = selection
+    let newSelections = [...state.selections, emptyDate(i + 1)]
+    setState(
+      {
+        selections: newSelections
+      }
+    )
+  }
+
+  let ranges = state.selections
+  let colors = new Array(ranges.length)
+  colors.fill("green")
   return (
     <div className="App">
-      <DateRangePicker
-        onChange={item => setState({ ...state, ...item })}
-        ranges={[state.selection1, state.selection2, state.selection3]}
+      <DateRange
+        onChange={onChange}
+        ranges={ranges}
+        scroll={{enabled: true}}
+        months={3}
+        rangeColors={colors}
+        minDate={new Date() }
+        showDateDisplay={false}
+        staticRanges={[]}
+        inputRanges={[]}
+        focusedRange = {[ranges.length - 1,0]}
+//        onRangeFocusChange={e => console.log("onRangeFocusChange") && console.log(e)}
+//        onPreviewChange={e => console.log("onPreviewChange") && console.log(e)}
+//        onShownDateChange={e => console.log("onShownDateChange") && console.log(e)}
       />
+      <ol>
+        {
+          ranges.map(range =>
+            <li key={range.key}>
+              {(range.startDate && DateTime.fromJSDate(range.startDate).toISODate())}
+              {" - "}
+               {(range.endDate && DateTime.fromJSDate(range.endDate).toISODate())}
+            </li>
+          )
+        }
+      </ol>
     </div>
   );
 }
